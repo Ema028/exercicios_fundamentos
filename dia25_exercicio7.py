90e1ba0f8755675172f27303c4ed8a462122cc78
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-03-25 10:57:46 -0300

    Update dia25_exercicio7.py

--- dia25_exercicio7.py
+++ dia25_exercicio7.py
@@ -3,5 +3,5 @@ valor = int(input("Qual o valor?"))
 nota_100 = valor // 100
 nota_50 = (valor - (nota_100 * 100)) // 50
 nota_20 = (valor - (nota_100 * 100) - (nota_50 * 50) // 20
-nota_10 = (valor - (nota_100 * 100) - (nota_50 - nota_20) // 10
+nota_10 = (valor - (nota_100 * 100) - (nota_50 * 50) - (nota_20 * 20) // 10
 print(f"Você vai precisar de {nota_100} notas de 100, {nota_50} de 50, {nota_20} de 20, {nota_10}")
