e7f6335a2b649a75c2ec59f2de800cffe4247b85
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-03-25 10:55:37 -0300

    Update and rename dia25_exercicio7 to dia25_exercicio7.py

--- dia25_exercicio7.py
+++ dia25_exercicio7.py
@@ -1,7 +1,7 @@
 print("Separador de notas")
 valor = int(input("Qual o valor?"))
 nota_100 = valor // 100
-nota_50 = (valor - nota_100) // 50
-nota_20 = (valor - nota_100 - nota_50) // 20
-nota_10 = (valor - nota_100 - nota_50 - nota_20) // 10
+nota_50 = (valor - (nota_100 * 100)) // 50
+nota_20 = (valor - (nota_100 * 100) - (nota_50 * 50) // 20
+nota_10 = (valor - (nota_100 * 100) - (nota_50 - nota_20) // 10
 print(f"Você vai precisar de {nota_100} notas de 100, {nota_50} de 50, {nota_20} de 20, {nota_10}")
