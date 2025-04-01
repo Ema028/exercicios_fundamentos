df7954321773dc70ac5f97d8a309a36921cad44f
Ema028 <156372524+Ema028@users.noreply.github.com>
2025-04-01 10:07:02 -0300

    Update primo.py

--- dia1/primo.py
+++ dia1/primo.py
@@ -1,6 +1,6 @@
 number = input("Digite um número\n")
 while number.isdigit() == False:
-    number = input("digite um número\n")
+    number = input("Digite um número\n")
 for i in range(2, int(number)):
     if int(number) % i == 0:
         print(f"o número {number} não é primo")
